6c095a1e702dee70693f181472de406a9d48cc86
MickaelE <mickael@mickenet.com>
2016-11-04 13:37:35 +0100

    Change in index

--- rebuild_composite_index.sql
+++ rebuild_composite_index.sql
@@ -1,9 +1,11 @@
-
-set head off pagesize 0 linesize 100
-
-select 'ALTER INDEX ' || index_owner || '.' || index_name || ' REBUILD SUBPARTITION ' ||
-subpartition_name || ';'
-from dba_ind_subpartitions
-where  index_owner='BPHADMIN'
-and status !='USABLE'
-/
+SET head OFF pagesize 0 linesize 100
+SELECT 'ALTER INDEX '
+  || index_owner
+  || '.'
+  || index_name
+  || ' REBUILD SUBPARTITION '
+  || subpartition_name
+  || ';'
+FROM dba_ind_subpartitions
+WHERE index_owner='BPHADMIN'
+AND status      !='USABLE' /
--- rebuild_non_composite_index.sql
+++ rebuild_non_composite_index.sql
@@ -1,16 +1,12 @@
-alter index "BPHADMIN"."I_INTCHG_DISPLAYID" REBUILD  PARTITION "SYS_P54062" ;
-
-select * from dba_ind_partitions
-select 'alter index '|| index_owner||'.'||index_name ||' REBUILD  PARTITION  "' || partition_name || '";' 
-from dba_ind_partitions where index_owner='BPHADMIN' and status !='USABLE';
-
-select * from dba_ind_partitions where index_name='I_INTCHG_DISPLAYID';
-
-set head off pagesize 0 linesize 100
-
-select 'ALTER INDEX ' || index_owner || '.' || index_name || ' REBUILD SUBPARTITION ' ||
-subpartition_name || ';'
-from dba_ind_subpartitions
-where  index_owner='BPHADMIN'
-and status !='USABLE'
-/
+SET head OFF pagesize 0 linesize 100
+SELECT 'alter index '
+  || index_owner
+  ||'.'
+  ||index_name
+  ||' REBUILD  PARTITION  "'
+  || partition_name
+  || '";'
+FROM dba_ind_partitions
+WHERE index_owner='BPHADMIN'
+AND status      !='USABLE'
+AND composite    ='NO';
